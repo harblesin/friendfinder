86eeef89a108135060f8698b96039927fbc9832a
h f <waltermelonson@gmail.com>
2019-05-09 14:09:22 -0700

    modal is functioning, needs to populate it within the post rather than a new GET call, began logic for determining best match based on total difference

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,6 +3,70 @@ var friends = [
         "name": "Tim",
         "photo": "https://www.hawaiinewsnow.com/resizer/SR3L2yhxNuY-rHSyFIQ9isJjdM8=/1200x600/arc-anglerfish-arc2-prod-raycom.s3.amazonaws.com/public/GAYFIY3ZZREPBOPKZQXYRNZ47Q.PNG",
         "scores": [1,1,1,1,2,3,1,2,5,1]
+    },
+    {
+        'name': "Joe",
+        'photo': 'https://pbs.twimg.com/profile_images/637120385092292608/x_ca9wLa_400x400.jpg',
+        'scores': [1,5,4,2,1,4,3,4,1,2]  
+    },
+    {
+        'name': "Steve",
+        'photo': 'https://www.famousbirthdays.com/faces/gatto-joe-image.jpg',
+        'scores': [2,3,2,1,3,2,1,1,2,4] 
+    },{
+        'name': "Mike",
+        'photo': 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Joe_Alwyn-2018.jpg/220px-Joe_Alwyn-2018.jpg',
+        'scores': [2,2,4,3,4,1,2,5,3,5] 
+    },
+    {
+        'name': "Bert",
+        'photo': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjDiqEkA_21VmJfgs6bvRXawo3pEw66SevyvbUcriwMvCAjsYH',
+        'scores': [5,3,1,2,4,1,5,4,2,5] 
+    },
+    {
+        'name': "Cathy",
+        'photo': 'https://learnlaunch.org/wp-content/uploads/2017/10/Untitled-design-6-300x300.png',
+        'scores': [3,4,2,4,5,1,3,4,5,2] 
+    },
+    {
+        'name': "Megan",
+        'photo': 'http://arvinas.com/wp-content/uploads/2018/09/Angela-250x250.jpg',
+        'scores': [3,2,1,4,2,3,1,2,2,4] 
+    },
+    {
+        'name': "Stephanie",
+        'photo': 'https://robertsonscholars.org/wp-content/uploads/2013/08/Little-Photo-300x300.jpeg',
+        'scores': [2,4,1,2,1,2,1,1,4,2] 
+    },
+    {
+        'name': "John Legend",
+        'photo': 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/John_Legend_2019_WBLS_Interview_1.png/220px-John_Legend_2019_WBLS_Interview_1.png',
+        'scores': [2,2,2,2,2,2,2,2,2,2] 
+    },
+    {
+        'name': "Phil",
+        'photo': 'https://www.citusdata.com/assets/images/team/hanefi-a1a75079.jpg',
+        'scores': [4,2,3,4,1,2,3,4,4,2] 
+    },
+    {
+        'name': "Migraine",
+        'photo': 'http://www.johanmathson.com/wordpress/wp-content/uploads/2014/11/stress-1.jpg',
+        'scores': [1,1,1,1,1,1,1,1,1,1] 
+    },
+    {
+        'name': "Karl",
+        'photo': 'https://redefineschool.files.wordpress.com/2016/07/karl-marx.png?w=367&h=481',
+        'scores': [4,4,4,4,4,4,4,4,4,4] 
+    },
+    {
+        'name': "Cheese",
+        'photo': 'https://media.istockphoto.com/photos/cheddar-cheese-being-cut-on-white-background-picture-id113583993?k=6&m=113583993&s=612x612&w=0&h=Cbbw5h6jCb2Bm5kw1yX_lYbcoKHmQrpGJR9NMs2_w7k=',
+        'scores': [3,3,3,3,3,3,3,3,3,3] 
+    },
+    {
+        'name': "Tom Cruise",
+        'photo': 'https://pbs.twimg.com/media/C6PosoiUoAEWIkC.jpg',
+        'scores': [5,5,5,5,5,5,5,5,5,5] 
     }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,9 +1,20 @@
-var friends = require("../data/friends.js")
+var friends = require("../data/friends.js");
+var match = require("../data/friends.js");
 
 module.exports = function(app){
 
 app.post('/api/friends', (req,res) => {
     var newFriend = req.body;
+
+    function difference(friendScores, userScores){
+        let totalDifference = 0;
+        for(var i = 0; i < userScores.scores.length; i++){
+            totalDifference += (friendScores.scores[i] - userScores.scores[i])
+            
+    }
+    }
+
+    
     friends.push(newFriend);
 
     res.json(newFriend);
