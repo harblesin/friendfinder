7dfc1bc45ad1f4ec349fd164fb607d6681f330ea
ylech <waltermelonson@gmail.com>
2019-05-09 21:19:36 -0700

    added in theory the logic for determining the best match, trying to test but having post error

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,26 +1,34 @@
 var friends = require("../data/friends.js");
 var match = require("../data/friends.js");
 
-module.exports = function(app){
+module.exports = function (app) {
 
-app.post('/api/friends', (req,res) => {
-    var newFriend = req.body;
+    app.post('/api/friends', (req, res) => {
+        var newFriend = req.body;
+        let match;
 
-    function difference(friendScores, userScores){
-        let totalDifference = 0;
-        for(var i = 0; i < userScores.scores.length; i++){
-            totalDifference += (friendScores.scores[i] - userScores.scores[i])
-            
-    }
-    }
+        function difference(friendScores, userScores) {
+            var userTotal;
+            let friendTotals =  [];
+            for (var i = 0; i < userScores.scores.length; i++) {
 
-    
-    friends.push(newFriend);
+                userTotal += Math.abs((userScores.scores[i]));
+                    return userTotal;
+            };
+            for (var j = 0; j < friendScores.scores.length;j++){
+                friendTotals.push(Math.abs(friendScores.scores[i] - userTotal));
+            };
+            return match = Math.min(friendTotals);
+        }
 
-    res.json(newFriend);
-});
+        difference(friends, newFriend);
 
-app.get('/api/friends', (req, res) => {
-    return res.json(friends);
-});
+        //friends.push(newFriend);
+
+        res.json(friends[math]);
+    });
+
+    app.get('/api/friends', (req, res) => {
+        return res.json(friends);
+    });
 };
