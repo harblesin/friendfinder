86fce0d93390d6d802fbe4d135d6d592e1116c5c
h f <waltermelonson@gmail.com>
2019-05-10 14:00:51 -0700

    reworking logic for displaying best match, issue with scope

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,31 +1,35 @@
 var friends = require("../data/friends.js");
-var match = require("../data/friends.js");
+//var match = require("../data/friends.js");
 
 module.exports = function (app) {
 
     app.post('/api/friends', (req, res) => {
         var newFriend = req.body;
-        let match;
+        //let match;
 
-        function difference(friendScores, userScores) {
-            var userTotal;
-            let friendTotals =  [];
-            for (var i = 0; i < userScores.scores.length; i++) {
+        var userTotal = 0;
+        let friendTotals = [];
+        for (var i = 0; i < newFriend.scores.length; i++) {
+            userTotal += Math.abs(parseInt(newFriend.scores[i]));
+            
+            friendTotals.push(parseInt(friends[0].scores[i]) - userTotal);
+        };
 
-                userTotal += Math.abs((userScores.scores[i]));
-                    return userTotal;
+        // for (var j = 0; j < friends.length; j++) {
+        //     let holder = friends[j];
+            
+        // };
+        for(var k = 0; k < friends[k].scores.length; k++){
+                //friendTotals.push(Math.abs(parseInt(friends[k].scores[k]) - userTotal));
+                let holder; 
+                
             };
-            for (var j = 0; j < friendScores.scores.length;j++){
-                friendTotals.push(Math.abs(friendScores.scores[i] - userTotal));
-            };
-            return match = Math.min(friendTotals);
-        }
-
-        difference(friends, newFriend);
+        var match = Math.min(friendTotals);
 
         //friends.push(newFriend);
-
-        res.json(friends[math]);
+        
+        //res.json(friends[match]);
+        res.json(friendTotals)
     });
 
     app.get('/api/friends', (req, res) => {
