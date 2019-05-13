99c3b344375c1d186250bec4b30ec4dc95506712
h f <waltermelonson@gmail.com>
2019-05-12 17:31:52 -0700

    final fix

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,7 @@
 var friends = [
     {
         "name": "Tim",
-        "photo": "https://www.hawaiinewsnow.com/resizer/SR3L2yhxNuY-rHSyFIQ9isJjdM8=/1200x600/arc-anglerfish-arc2-prod-raycom.s3.amazonaws.com/public/GAYFIY3ZZREPBOPKZQXYRNZ47Q.PNG",
+        "photo": "http://www.rcvs.org.uk/public/images/tim-greet.jpg",
         "scores": [1,1,1,1,2,3,1,2,5,1]
     },
     {
